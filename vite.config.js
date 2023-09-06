977b44dbe7df1ab3c5a2b2c9d1db20fe68d2325a
Hadhad / HanoHano <katai5plate.sub@gmail.com>
2023-09-06 23:10:28 +0900

    ビルドからハッシュ削除

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ export default {
   base: "./",
   build: {
     outDir: "docs",
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
   },
 };
